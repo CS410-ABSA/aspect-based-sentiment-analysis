798411d3528c061c12e52dbe500f0c3def49bedd
ekraihan <ekraihan@iupui.edu>
2018-12-08 11:52:26 -0500

    adding back preprocessing

--- training/absa_sentiments.py
+++ training/absa_sentiments.py
@@ -11,6 +11,19 @@ num_classes = 5
 
 #sentences = ['this does not work', 'this is an ok product']
 
+def preprocess(paragraph):
+    sentences = tokenize.sent_tokenize(paragraph.lower())
+    lemma = WordNetLemmatizer()
+    clean_sentences = []
+    for sentence in sentences:
+        sentence = re.sub('[^\w\s]','',sentence)
+        lemma_sent = []
+        for word in sentence.split():
+            word = lemma.lemmatize(word)
+            lemma_sent.append(word)
+        clean_sentences.append(' '.join(lemma_sent))
+    return clean_sentences
+    
 def sentiments(sentences):
     model = load_model('cnn_absa_model.h5')
     lb = joblib.load('cnn_label_binarizer.joblib')
