41d68edfbf7ccadf8206d97db5d75d3b94262dbe
ekraihan <ekraihan@iupui.edu>
2018-12-08 11:47:06 -0500

    adding some topic extraction

--- topic_segmentation/segment.py
+++ topic_segmentation/segment.py
@@ -4,20 +4,31 @@ import sys
 from gensim.corpora import Dictionary
 from gensim.models.ldamodel import LdaModel
 from gensim.test.utils import datapath
+from nltk import tokenize
 
-review = open('topic_segmentation/review.txt', 'r')
+review = open('review.txt', 'r').read()
 
-sentences = preprocessParagraph(review.read())
+sentences = preprocessParagraph(review)
 
 dictionary = Dictionary(sentences)
+
+# scikit learn: tdf vectorizor
+# also try to use idf
 corpus = [dictionary.doc2bow(sentence) for sentence in sentences]
 
-lda = LdaModel(corpus, num_topics=5, passes=15, id2word=dictionary)
-# temp_file = datapath("model")
-# lda.save(temp_file)
+lda = LdaModel(corpus, num_topics=10, chunksize=20, passes=50, id2word=dictionary)
 
-
-# lda = LdaModel.load(temp_file)
 topics = lda.print_topics(num_words=4)
-for topic in topics:
-    print(topic)
+
+doc_scores = [lda.get_document_topics(document) for document in corpus]
+
+def max_by_index(array, index):
+    return max(array, key=lambda item: item[index])
+
+
+doc_topics = [max_by_index(doc_score,1) for doc_score in doc_scores]
+
+raw_sentences = tokenize.sent_tokenize(review)
+
+first_topic_sentences = [raw_sentences[i] for i,tuple_ in enumerate(doc_topics)
+                            if tuple_[0] == 1]
