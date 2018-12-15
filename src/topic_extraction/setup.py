f16c9874834eafb234c4ee3c32bc85abd346ba88
ekraihan <ekraihan@iupui.edu>
2018-12-15 18:55:24 -0500

    adding setup again

--- src/topic_extraction/setup.py
+++ src/topic_extraction/setup.py
@@ -3,3 +3,4 @@ import nltk
 
 # Download dir for elastic beanstalk
 nltk.download('wordnet', download_dir='/usr/local/lib/nltk_data')
+nltk.download('stopwords', download_dir='/usr/local/lib/nltk_data')
