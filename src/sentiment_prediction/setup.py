4eb5c1f60389c320cde8572891aea332b472bf03
ekraihan <ekraihan@iupui.edu>
2018-12-15 14:51:53 -0500

    adding path

--- src/sentiment_prediction/setup.py
+++ src/sentiment_prediction/setup.py
@@ -4,17 +4,17 @@ import shutil
 import os
 
 print("Retrieving 1")
-urllib.request.urlretrieve("https://s3.amazonaws.com/absa-models/bin/bow_sa_pipeline.joblib", "../../models/bow_sa_pipeline.joblib")
+urllib.request.urlretrieve("https://s3.amazonaws.com/absa-models/bin/bow_sa_pipeline.joblib", "./models/bow_sa_pipeline.joblib")
 print("Retrieving 2")
-urllib.request.urlretrieve("https://s3.amazonaws.com/absa-models/bin/cnn_absa_model.h5", "../../models/cnn_absa_model.h5")
+urllib.request.urlretrieve("https://s3.amazonaws.com/absa-models/bin/cnn_absa_model.h5", "./models/cnn_absa_model.h5")
 print("Retrieving 3")
-urllib.request.urlretrieve("https://s3.amazonaws.com/absa-models/bin/cnn_label_binarizer.joblib", "../../models/cnn_label_binarizer.joblib")
+urllib.request.urlretrieve("https://s3.amazonaws.com/absa-models/bin/cnn_label_binarizer.joblib", "./models/cnn_label_binarizer.joblib")
 print("Retrieving 4")
-urllib.request.urlretrieve("https://s3.amazonaws.com/absa-models/bin/word2vec.model", "../../models/word2vec.model")
+urllib.request.urlretrieve("https://s3.amazonaws.com/absa-models/bin/word2vec.model", "./models/word2vec.model")
 print("Retrieving 5")
-urllib.request.urlretrieve("https://s3.amazonaws.com/absa-models/bin/word2vec.model.trainables.syn1neg.npy", "../../models/word2vec.model.trainables.syn1neg.npy")
+urllib.request.urlretrieve("https://s3.amazonaws.com/absa-models/bin/word2vec.model.trainables.syn1neg.npy", "./models/word2vec.model.trainables.syn1neg.npy")
 print("Retrieving 6")
-urllib.request.urlretrieve("https://s3.amazonaws.com/absa-models/bin/word2vec.model.wv.vectors.npy", "../../models/word2vec.model.wv.vectors.npy")
+urllib.request.urlretrieve("https://s3.amazonaws.com/absa-models/bin/word2vec.model.wv.vectors.npy", "./models/word2vec.model.wv.vectors.npy")
 
 # with gzip.open('amazon_reviews_us_Electronics_v1_00.tsv.gz', 'rb') as f_in:
 #     with open('amazon_reviews_us_Electronics_v1_00.tsv', 'wb') as f_out:
@@ -25,4 +25,3 @@ urllib.request.urlretrieve("https://s3.amazonaws.com/absa-models/bin/word2vec.mo
 #     os.remove("amazon_reviews_us_Electronics_v1_00.tsv.gz")
 # else:
 #     print("The file does not exist")
-    
