cff1926e3f1779afe1d243754191292fe1fb66b7
ekraihan <ekraihan@iupui.edu>
2018-12-15 23:48:46 -0500

    fixing tensorflow problem

--- src/topic_extraction/setup.py
+++ src/topic_extraction/setup.py
@@ -4,3 +4,4 @@ import nltk
 # Download dir for elastic beanstalk
 nltk.download('wordnet', download_dir='/usr/local/lib/nltk_data')
 nltk.download('stopwords', download_dir='/usr/local/lib/nltk_data')
+nltk.download('punkt', download_dir='/usr/local/lib/nltk_data')
